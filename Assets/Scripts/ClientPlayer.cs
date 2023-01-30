64b0887115e6bd4b68a0c6d7053784de44aa2276
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-01-30 14:59:13 +0900

    Input Manager

--- Assets/Scripts/ClientPlayer.cs
+++ Assets/Scripts/ClientPlayer.cs
@@ -4,17 +4,8 @@ using UnityEngine;
 
 public class ClientPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private float walkingSpeed = 1f;
+    [SerializeField] private float runningSpeed = 3f;
 
     //Control the Animator. Highly depend on the structure of Animator.
     private class AnimatorController
