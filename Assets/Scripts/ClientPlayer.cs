35cfc9ff0253b7d3b63116a5af95197493b0e333
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-01-30 18:14:48 +0900

    falling

--- Assets/Scripts/ClientPlayer.cs
+++ Assets/Scripts/ClientPlayer.cs
@@ -1,9 +1,29 @@
 using UnityEngine;
 
+/// <summary>
+/// Control the client player
+/// including gravity control.
+/// This falls to -transform.up
+/// Has GroundedChecker beneath
+/// </summary>
 public class ClientPlayer : MonoBehaviour
 {
+    [Header("Physical consts")]
     [SerializeField] private float walkingSpeed = 1f;
     [SerializeField] private float runningSpeed = 3f;
+    [SerializeField] private float jumpHeight = 10f;
+    [Tooltip("Gravitational Acceleration")]
+    [SerializeField] private float gravitation = -0.5f;
+    [SerializeField] private float minFallingSpeed = -2f;
+
+    [Header("Grounded Checker")]
+    [SerializeField] private float groundedCheckerRadius = 1f;
+    [SerializeField] private float groundedCheckerOffset = -1f;
+    [SerializeField] private LayerMask groundLayer;
+
+    [Header("Adjusting Ground")]
+    [Tooltip("The max vertical distance of player from the ground")]
+    [SerializeField] private float adjusterRadius = 0.1f;
 
     public enum State
     {
@@ -13,26 +33,49 @@ public class ClientPlayer : MonoBehaviour
     }
 
     public State state { set; private get; } = State.Idle;
+    
+    //the state will be Idle if this is true for the whole frame
+    private bool willBeIdle = false;
+
+    //for jumping + gravity
+    private float verticalSpeed = 0f;
 
     private AnimatorController animatorController;
 
     void Start()
     {
-        animatorController
-            = new AnimatorController(
-                GetComponent<Animator>()
-              );
+        animatorController = new AnimatorController(
+                                GetComponent<Animator>()
+                              );
     }
 
     private void Update()
     {
-        //If the state is other than Idle,
-        //state must be changed
-        //when other functions such as WalkForward() called every frame
-        State currentState = state;
-        state = State.Idle;
+        //check if grounded and reflect gravity
+        Fall();
 
-        animatorController.Update(currentState);
+        //If the state is changed other than Idle,
+        //this must be false
+        //as long as other functions such as WalkForward() called every frame
+        if (willBeIdle)
+        {
+            state = State.Idle;
+        }
+        willBeIdle = true;
+
+        //update animation
+        animatorController.Update(state);
+    }
+
+    /// <summary>
+    /// Change State
+    /// </summary>
+    private void ChangeState(State newState)
+    {
+        state = newState;
+
+        //stop being Idle
+        willBeIdle = false;
     }
 
     //Called by InputManager
@@ -43,7 +86,9 @@ public class ClientPlayer : MonoBehaviour
         Translate(vec);
 
         //change states
-        this.state = State.Moving;
+        if ((state == State.Idle) || (state == State.Moving)) {
+            ChangeState(State.Moving);
+        }
         animatorController.movingState = AnimatorController.MovingState.Walking;
     }
 
@@ -55,7 +100,10 @@ public class ClientPlayer : MonoBehaviour
         Translate(vec);
 
         //change states
-        this.state = State.Moving;
+        if ((state == State.Idle) || (state == State.Moving))
+        {
+            ChangeState(State.Moving);
+        }
         animatorController.movingState = AnimatorController.MovingState.Running;
     }
 
@@ -65,6 +113,115 @@ public class ClientPlayer : MonoBehaviour
     private void Translate(Vector3 velocity)
     {
         transform.position += velocity * Time.deltaTime;
+    }
+
+    /// <summary>
+    /// Check if in air or not
+    /// And get gravity
+    /// </summary>
+    private void Fall()
+    {
+        bool grounded = CheckGrounded();
+
+        if (!grounded)
+        {
+            //in air
+
+            //fall down
+            Vector3 vec = transform.up * verticalSpeed;
+            Translate(vec);
+
+            //accerlate
+            verticalSpeed += gravitation * Time.deltaTime;
+
+            //shouldn't exceed min
+            if (verticalSpeed < minFallingSpeed)
+            {
+                verticalSpeed = minFallingSpeed;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Check if grounded.
+    /// If not grounded, the state will be Jumping.
+    /// Also adjust when landed
+    /// </summary>
+    private bool CheckGrounded()
+    {
+        bool grounded = Physics.CheckSphere(
+                            GetGroundedCheckerPosition(),
+                            groundedCheckerRadius,
+                            groundLayer
+                        );
+
+        //if this is the first frame of grounded
+        if(grounded && (state == State.Jumping))
+        {
+            AdjustGround();
+            state = State.Idle;
+        }
+
+        if (!grounded)
+        {
+            ChangeState(State.Jumping);
+        }
+
+        return grounded;
+    }
+
+    /// <summary>
+    /// Adjust the vertical position of the character
+    /// so as it right at the ground, not buried or floating
+    /// Should be called when grounded == true
+    /// </summary>
+    private void AdjustGround()
+    {
+        Vector3 initialFootPosition = GetGroundedCheckerPosition();
+
+        float optimalZ = 0f;
+
+        //start from the edge to the other edge of the GroundChecker
+        for(float z = -groundedCheckerRadius; z <= groundedCheckerRadius; z += adjusterRadius)
+        {
+            Vector3 adjustedPosition
+                = initialFootPosition + transform.up * z;
+
+            //Use the adjuster
+            bool collided
+                = Physics.CheckSphere(
+                        adjustedPosition,
+                        adjusterRadius,
+                        groundLayer
+                    );
+
+            if (collided)
+            {
+                //adjust completed
+                optimalZ = z;
+                break;
+            }
+        }
+
+        //Adjust the position
+        transform.position += transform.up * optimalZ;
+    }
+
+    //for debugging grounded checker
+    private void OnDrawGizmosSelected()
+    {
+        //show grounded checker
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawSphere(GetGroundedCheckerPosition(), groundedCheckerRadius);
+
+        //show adjuster
+        Gizmos.color = new Color(0, 0, 1, 1f);
+        Gizmos.DrawSphere(GetGroundedCheckerPosition(), adjusterRadius);
+    }
+
+    private Vector3 GetGroundedCheckerPosition()
+    {
+        return transform.position + transform.up.normalized * groundedCheckerOffset;
     }
 
     /// <summary>
