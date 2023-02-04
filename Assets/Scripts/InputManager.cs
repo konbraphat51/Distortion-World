853a3123458c2e1dc469abbca16b395e5a341179
konbraphat51 <brightray48@zeus.eonet.ne.jp>
2023-02-04 13:22:56 +0900

    button + function application + dontWait

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -8,7 +8,7 @@ using UnityEngine.Events;
 /// <summary>
 /// Invoke each UnityEvent when corresponding key pushed
 /// </summary>
-public class InputManager : MonoBehaviour
+public class InputManager : SingletonMonoBehaviour<InputManager>
 {
     //Simply push
     [SerializeField] private KeyAction[] simpleActions;
@@ -19,7 +19,26 @@ public class InputManager : MonoBehaviour
     [Tooltip("longest interval of several pushes(s)")]
     [SerializeField] private float pushesInterval = 0.3f;
 
+    //get time of interval of key pushes 
     private List<KeyTimer> keyTimers = new List<KeyTimer>();
+
+    //remember whether the key is pushed
+    //keyname -> isPushed
+    private Dictionary<string, bool> isPushed = new Dictionary<string, bool>();
+
+    //remember whether the key was pushed 1 frame before
+    //keyname -> isPushed
+    private Dictionary<string, bool> wasPushed = new Dictionary<string, bool>();
+
+    //pushed by Push() (update each frame)
+    //contains keyName
+    private List<string> functionPushed = new List<string>();
+
+    //remember corresponding key to avoid duplicated search at PushForAction()
+    private Dictionary<UnityAction, string> actionToKey = new Dictionary<UnityAction, string>();
+
+    [Tooltip("Button Name -> Key Name")]
+    [SerializeField] private Dictionary<string, string> buttonsInterpretations = new Dictionary<string, string>();
 
     //contains infomations of several push keys
     private class KeyTimer
@@ -29,6 +48,9 @@ public class InputManager : MonoBehaviour
 
         //time passed after the latest push
         public float timer = 0f;
+
+        //time passed after the first push
+        public float totalTimer = 0f;
 
         //contructor
         public KeyTimer(string keyName)
@@ -50,53 +72,108 @@ public class InputManager : MonoBehaviour
     private void Start()
     {
         InitializeSeveralPushesKeys();
+
+        UpdateIsPushedIndex();
     }
 
     private void Update()
     {
-        CheckKeys();        
+        UpdateKeys();
+        RunActions();        
     }
 
     /// <summary>
-    /// Check key is pushed and process it if pushed
+    /// Update isPushed by GetKey + GetButton + Push functions
     /// </summary>
-    private void CheckKeys()
+    private void UpdateKeys()
     {
-        CheckSimpleActions();
+        //update wasPushed (1 frame before)
+        wasPushed = new Dictionary<string, bool>(isPushed);
+
+        //reset
+        foreach (string key in wasPushed.Keys)
+        {
+            isPushed[key] = false;
+        }
+
+        //check key
+        foreach(string keyName in wasPushed.Keys)
+        {
+            if (Input.GetKey(keyName))
+            {
+                isPushed[keyName] = true;
+            }
+        }
+
+        //check buttons
+        foreach(string buttonName in buttonsInterpretations.Keys)
+        {
+            if (Input.GetButton(buttonName))
+            {
+                isPushed[buttonsInterpretations[buttonName]] = true;
+            }
+        }
+
+        //check function Push
+        foreach(string keyName in functionPushed)
+        {
+            isPushed[keyName] = true;
+        }
+
+        //reset
+        functionPushed = new List<string>();
+    }
+
+    /// <summary>
+    /// Push by functions
+    /// ex) for EventTrigger
+    /// </summary>
+    public static void Push(string keyName)
+    {
+        Instance.functionPushed.Add(keyName);
+    }
+
+    /// <summary>
+    /// Run actions if pushed
+    /// </summary>
+    private void RunActions()
+    {
+        RunSimpleActions();
         CheckSeveralPushesIncreases();
 
         CheckTimers();
 
-        CheckSeveralPushes();
+        RunSeveralPushesActions();
     }
 
     /// <summary>
     /// check if pushed and invoke of simpleActions
     /// </summary>
-    private void CheckSimpleActions()
+    private void RunSimpleActions()
     {
         foreach(KeyAction keyAction in simpleActions)
         {
             //correspond to input type
             switch (keyAction.inputType)
             {
-                case 0:
+                case KeyAction.KeyType.GetKey:
                     //Invoke during pushed
-                    if(Input.GetKey(keyAction.keyName)){
+                    if(GetKey(keyAction.keyName)){
                         keyAction.Invoke();
                     }
                     break;
 
-                case 1:
+                case KeyAction.KeyType.GetKeyDown:
                     //Invoke right on the time of pushed
-                    if(Input.GetKeyDown(keyAction.keyName)){
+                    if(GetKeyDown(keyAction.keyName)){
                         keyAction.Invoke();
                     }
                     break;
 
-                case 2:
+                case KeyAction.KeyType.GetKeyUp:
                     //Invoke right on the time key up
-                    if(Input.GetKeyUp(keyAction.keyName)){
+                    if (GetKeyUp(keyAction.keyName))
+                    {
                         keyAction.Invoke();
                     }
                     break;
@@ -111,7 +188,7 @@ public class InputManager : MonoBehaviour
     {
         foreach (string keyName in severalPushesKeys)
         {
-            if (Input.GetKeyDown(keyName))
+            if (GetKeyDown(keyName))
             {
                 //counter increases
                 KeyTimer keyTimer = GetTimer(keyName);
@@ -119,8 +196,34 @@ public class InputManager : MonoBehaviour
 
                 //reset timer
                 keyTimer.timer = 0f;
+
+                ////run immedietly
+                KeyActionSeveralPushes correspondingAction = GetSeveralPushesAction(keyName, keyTimer.pushedNum);
+                //if exists and dont wait
+                if (correspondingAction != null && correspondingAction.dontWait)
+                {
+                    runningActions.Add(correspondingAction);
+                }
             }
         }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private KeyActionSeveralPushes GetSeveralPushesAction(string keyName, int pushesNum)
+    {
+        foreach(KeyActionSeveralPushes keyAction in severalPushesActions)
+        {
+            if ((keyAction.keyName == keyName) && (keyAction.pushesNum == pushesNum))
+            {
+                //found
+                return keyAction;
+            }
+        }
+
+        //couldn't find
+        return null;
     }
 
     /// <summary>
@@ -163,6 +266,18 @@ public class InputManager : MonoBehaviour
             {
                 //exceeded
 
+                //get rid of same key action if exists
+                List<KeyActionSeveralPushes> _runningActions
+                    = new List<KeyActionSeveralPushes>(runningActions);
+                foreach (KeyActionSeveralPushes action in _runningActions)
+                {
+                    if (action.keyName == keyTimer.keyName)
+                    {
+                        //found
+                        runningActions.Remove(action);
+                    }
+                }
+
                 ////start running
                 //get corresponding action
                 foreach (KeyActionSeveralPushes action in severalPushesActions)
@@ -186,7 +301,7 @@ public class InputManager : MonoBehaviour
     /// <summary>
     /// run the severalPushes
     /// </summary>
-    private void CheckSeveralPushes()
+    private void RunSeveralPushesActions()
     {
         List<KeyActionSeveralPushes> _runningActions
             = new List<KeyActionSeveralPushes>(runningActions);
@@ -194,9 +309,9 @@ public class InputManager : MonoBehaviour
         {
             switch (action.inputType)
             {
-                case 0:
+                case KeyAction.KeyType.GetKey:
                     //invoke while pushed
-                    if (Input.GetKey(action.keyName))
+                    if (GetKey(action.keyName))
                     {
                         action.Invoke();
                     }
@@ -207,17 +322,17 @@ public class InputManager : MonoBehaviour
                     }
                     break;
 
-                case 1:
+                case KeyAction.KeyType.GetKeyDown:
                     //run immedietly
                     action.Invoke();
                     runningActions.Remove(action);
                     break;
 
-                case 2:
+                case KeyAction.KeyType.GetKeyUp:
                     //run when up
                     //not using GetKeyUp because of possibility of
                     //key up during waiting for keyTimer
-                    if (!Input.GetKey(action.keyName))
+                    if (GetKeyUp(action.keyName))
                     {
                         action.Invoke();
                         runningActions.Remove(action);
@@ -244,18 +359,110 @@ public class InputManager : MonoBehaviour
     }
 
     /// <summary>
+    /// Check if there is index missing in isPushed, and add the missing index
+    /// </summary>
+    private void UpdateIsPushedIndex()
+    {
+        foreach(KeyAction keyAction in simpleActions)
+        {
+            if (!isPushed.ContainsKey(keyAction.keyName))
+            {
+                //doesn't contain
+                //add the index
+                isPushed[keyAction.keyName] = false;
+            }
+
+            if (!wasPushed.ContainsKey(keyAction.keyName))
+            {
+                //doesn't contain
+                //add the index
+                wasPushed[keyAction.keyName] = false;
+            }
+        }
+
+        foreach(KeyActionSeveralPushes keyAction in severalPushesActions)
+        {
+            if (!isPushed.ContainsKey(keyAction.keyName))
+            {
+                //doesn't contain
+                //add the index
+                isPushed[keyAction.keyName] = false;
+            }
+
+            if (!wasPushed.ContainsKey(keyAction.keyName))
+            {
+                //doesn't contain
+                //add the index
+                wasPushed[keyAction.keyName] = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// return true while pushed
+    /// </summary>
+    private bool GetKey(string keyName)
+    {
+        if (isPushed[keyName])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// return true only the first frame pushed
+    /// </summary>
+    private bool GetKeyDown(string keyName)
+    {
+        if (!wasPushed[keyName] && isPushed[keyName])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// return true only the first frame stop pushed
+    /// </summary>
+    private bool GetKeyUp(string keyName)
+    {
+        if (wasPushed[keyName] && !isPushed[keyName])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
     /// Actions and its key
     /// </summary>
     [System.Serializable] public class KeyAction
     {
+        public enum KeyType
+        {
+            GetKey,
+            GetKeyDown,
+            GetKeyUp
+        }
+
         [Tooltip("Methods called when key pushed")]
-        [SerializeField] private UnityEvent actions;
+        [SerializeField] private UnityEvent _actions;
 
         [Tooltip("Key name. Google 'Unity Input key name'")]
         [SerializeField] private string _keyName;
 
         [Tooltip("0:GetKey, 1:GetKeyDown, 2:GetKeyUp")]
-        [SerializeField] private byte _inputType = 0;
+        [SerializeField] private KeyType _inputType = 0;
         
         //getters. These are necessary for SerializeField
         public string keyName
@@ -266,11 +473,19 @@ public class InputManager : MonoBehaviour
             }
         }
 
-        public byte inputType
+        public KeyType inputType
         {
             get
             {
                 return _inputType;
+            }
+        }
+
+        public UnityEvent actions
+        {
+            get
+            {
+                return _actions;
             }
         }
 
@@ -292,12 +507,23 @@ public class InputManager : MonoBehaviour
         [Tooltip("How many pushes required")]
         [SerializeField] private int _pushesNum = 1;
 
+        [Tooltip("if true, the action invoked before next push. The invoke will cancel when next push appear")]
+        [SerializeField] private bool _dontWait = false;
+
         //getters
         public int pushesNum
         {
             get
             {
                 return _pushesNum;
+            }
+        }
+
+        public bool dontWait
+        {
+            get
+            {
+                return _dontWait;
             }
         }
     }
